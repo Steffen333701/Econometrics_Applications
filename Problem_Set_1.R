58f0400d088e89ecf528449afd04eec313a3a782
Steffen333701 <steffen.fenzl@googlemail.com>
2018-04-12 20:11:37 +0200

    Initial Commit of Problem set 1

---
+++ Problem_Set_1.R
@@ -0,0 +1,12 @@
+
+# Problem set 1
+
+
+# clean space and set wd
+
+ls()
+rm(list = ls())
+setwd("C:/Users/Steffen_Laptop/Documents/R/Econometrics_Applications")
+
+# load the data 
+datps1 <- read.csv(file = "ps1_nls.csv")
