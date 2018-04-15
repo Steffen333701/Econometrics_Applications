0106c87e26b96baf955319d671c8ed95d998a397
Steffen333701 <steffen.fenzl@googlemail.com>
2018-04-15 10:34:17 +0200

    finish this problem set

--- Problem_Set_1.R
+++ Problem_Set_1.R
@@ -67,8 +67,23 @@ reg.model.2 <- lm(logWage ~ educ + exper + I(exper^2), data = datps1)
 summary(reg.model.2)
 
 ## 8 ====================
+ 
+coeff.8<- data.frame(matrix(unlist(reg.model.2[1]), nrow=4, byrow=T))
+ern.8a <- coeff.8[1,1] + coeff.8[2,1] * 9  + coeff.8[3,1] * (40-9-6) + coeff.8[4,1] * (40-9-6)^2
+ern.8b <- coeff.8[1,1] + coeff.8[2,1] * 13  + coeff.8[3,1] * (40-13-6) + coeff.8[4,1] * (40-13-6)^2
+ern.8.diff = ern.8a - ern.8b
 
-# number 8
+
+## 9 ====================
+
+dat.ps1.refactored <- data.frame(datps1$logWage ,datps1$educ, datps1$educ + datps1$exper, datps1$educ*46 + datps1$exper^2)
+colnames(dat.ps1.refactored) <- c("A", "B", "C", "D")
+
+reg.model.5 <- lm(A ~ B+C+D, data=dat.ps1.refactored)
+summary(reg.model.5)
+
+coeff.9 = data.frame(matrix(unlist(reg.model.5[1]), nrow=4, byrow=T))
+ern.9.diff = coeff.9[2,1] * (9-13)
 
 ## 10 ===================
 reg.model.3 <- lm(logWage ~ educ + exper + I(exper^2) + age, data = datps1)
@@ -76,9 +91,19 @@ summary(reg.model.3)
 
 # Here one can see perfect multicollinearity
 
+
+## 11 ===================
+# Omitted variable bias
+# expect coefficient on education to become smaller
+# education has a positive influence on earnings and i expect education and iq to be positively correlated
+
+
 ## 12 ===================
 reg.model.4 <- lm(logWage ~ educ + iq, data = datps1)
 summary(reg.model.4)
+
+# i expect and 10 point iq increase to increase earnings by 4%
+
 
 
 
